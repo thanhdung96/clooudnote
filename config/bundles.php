31db7c04f374dbd74d34b44e570313d3af45e013
thanhdung96 <duduong96@outlook.com>
2024-12-17 22:45:51 +0700

    separated into bundles.
    Changes to be committed:
    	modified:   config/bundles.php
    	modified:   config/packages/doctrine.yaml
    	modified:   config/routes.yaml
    	modified:   config/services.yaml
    	new file:   src/ApiBundle/ApiBundle.php
    	renamed:    src/Controller/.gitignore -> src/ApiBundle/Controller/.gitignore
    	new file:   src/AppBundle/AppBundle.php
    	renamed:    src/Entity/.gitignore -> src/AppBundle/Entity/.gitignore
    	renamed:    src/Repository/.gitignore -> src/AppBundle/Repository/.gitignore
    	new file:   src/BaseBundle/BaseBundle.php

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    App\AppBundle\AppBundle::class => ['all' => true],
+    App\ApiBundle\ApiBundle::class => ['all' => true],
+    App\BaseBundle\BaseBundle::class => ['all' => true],
 ];
---
+++ src/AppBundle/AppBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class AppBundle extends AbstractBundle
+{
+
+}
